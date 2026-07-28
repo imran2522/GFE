f50349d54a1dff8509ab7818e7ed01655e0b9c41
Imran Ahmed Khan <imhysoft@gmail.com>
2026-07-28 15:23:21 +0500

    Updated classnames.js as per new test cases

--- GFE-75/03-Classnames.js
+++ GFE-75/03-Classnames.js
@@ -1,8 +1,13 @@
 export default function classNames(...args) {
     const classes = [];
+
     for (const arg of args) {
-        if (typeof arg === 'string') {
+        if (typeof arg === 'string' && arg) {
             classes.push(arg);
+        } else if (typeof arg === 'number' && arg !== 0) {
+            classes.push(arg);
+        } else if (Array.isArray(arg)) {
+            classes.push(classNames(...arg));
         } else if (typeof arg === 'object' && arg !== null) {
             for (const key in arg) {
                 if (arg[key]) {
@@ -11,9 +16,10 @@ export default function classNames(...args) {
             }
         }
     }
-    return classes.join(' ');   
+
+    return classes.filter(Boolean).join(' ');
 }
 
 // Example usage:
-const buttonClass = classNames('btn', { 'btn-primary': true, 'btn-disabled': false });
+const buttonClass = classNames('btn', ['btn-primary', { 'btn-disabled': false }]);
 console.log(buttonClass); // Output: "btn btn-primary"  
